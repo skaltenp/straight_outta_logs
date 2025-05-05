fc7c2cfdd29549efdd2c48039f73a67a44da555b
Sascha Kaltenpoth <skaltenp@gmail.com>
2025-05-05 18:24:39 +0200

    mccommity

--- 02_cvsft.sh
+++ 02_cvsft.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-accelerate launch --config_file gpu0.yaml sft.py --dataset_name skaltenp/sepsis_cases --fold_name cv_split0 &&
-accelerate launch --config_file gpu0.yaml sft.py --dataset_name skaltenp/sepsis_cases --fold_name cv_split1 &&
-accelerate launch --config_file gpu0.yaml sft.py --dataset_name skaltenp/sepsis_cases --fold_name cv_split2 &&
-accelerate launch --config_file gpu0.yaml sft.py --dataset_name skaltenp/sepsis_cases --fold_name cv_split3 &&
-accelerate launch --config_file gpu0.yaml sft.py --dataset_name skaltenp/sepsis_cases --fold_name cv_split4
+accelerate launch sft.py --fold_name cv_split0 &&
+accelerate launch sft.py --fold_name cv_split1 &&
+accelerate launch sft.py --fold_name cv_split2 &&
+accelerate launch sft.py --fold_name cv_split3 &&
+accelerate launch sft.py --fold_name cv_split4
